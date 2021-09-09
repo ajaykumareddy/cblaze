1ac5afda06b22dcdb4e8bdba4901b5f0c32b8af9
ajay kumar reddy <ajay.reddy@softsuave.com>
2021-09-09 14:54:21 +0530

    implemented change language on home page

--- src/components/ChangeLanguage/ChangeLanguage.jsx
+++ src/components/ChangeLanguage/ChangeLanguage.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import i18n from "i18next";
 
 
 function ChangeLanguage(props) {
 
+
     function changeLanguage(e) {
         i18n.changeLanguage(e.target.options[e.target.options.selectedIndex].value);
     }
+    const [options, addOptions] = useState([{ name: 'EN' ,value:"en" }, { name: 'TA' ,value:"ta" }]);
 
     return (<div className="ChangeLanguage">
         <select name="lang" onChange={changeLanguage}>
-            <option value="en">en</option>
-            <option value="ta">ta</option>
+            {options.map((option,index)=>{
+                return <option key={index} value={option.value}>{option.name}</option>
+            })}
         </select>
 
     </div>);
